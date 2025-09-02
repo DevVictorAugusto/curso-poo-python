df2d502f462e3f3c8661434b4fa8c8b924ce6da2
Victor Augusto <victorsantan435@gmail.com>
2025-09-02 16:29:27 -0300

    8 dia de estudos iniciando módulo de manipualçao de arquivos

---
+++ ManipuçãoArquivos/1-escrita.py
@@ -0,0 +1,19 @@
+
+
+name = input("Digite seu nome: \n")
+""""
+
+Arquivos :
+opção w -	write
+opção a -	append
+opção r	- read
+
+"""
+#Alternativa 1
+arquivo = open( "names.txt", "a")
+arquivo.write(f"{name}\n")
+arquivo.close()
+
+#Alternativa 2
+with open("names.txt", "a") as arquivo:
+    arquivo.write(f"{name}\n")
